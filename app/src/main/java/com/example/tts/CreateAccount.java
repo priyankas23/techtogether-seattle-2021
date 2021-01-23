f3ba925d0b35b0c0140bd37b2e531fd1a2db9cc7
natasha-png <natashav2472@gmail.com>
2021-01-23 15:02:32 +0530

    Second commit- for Create Account page

--- app/src/main/java/com/example/tts/CreateAccount.java
+++ app/src/main/java/com/example/tts/CreateAccount.java
@@ -1,4 +1,27 @@
 package com.example.tts;
 
-public class CreateAccount {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class CreateAccount extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.create_account);
+
+        DisplayMetrics wh = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(wh);
+
+        double width = wh.widthPixels;
+        double height = wh.heightPixels;
+
+        getWindow().setLayout((int) (width*.9), (int) (height*.4));
+
+
+
+
+    }
 }
